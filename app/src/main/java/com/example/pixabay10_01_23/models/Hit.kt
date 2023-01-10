6e03e6a001dfa4a3d0869aa877063981c8871ea6
Pablo Sánchez López <65461723+PSL2001@users.noreply.github.com>
2023-01-10 13:54:11 +0100

    Modelos creados; Creado layout de recycler

---
+++ app/src/main/java/com/example/pixabay10_01_23/models/Hit.kt
@@ -0,0 +1,7 @@
+package com.example.pixabay10_01_23.models
+
+data class Hit(
+    val webformatURL: String,
+    val user: String,
+    val likes: Int
+)
---
+++ app/src/main/java/com/example/pixabay10_01_23/models/Pixabay.kt
@@ -0,0 +1,5 @@
+package com.example.pixabay10_01_23.models
+
+data class Pixabay(
+    val hits: List<Hit>
+)
