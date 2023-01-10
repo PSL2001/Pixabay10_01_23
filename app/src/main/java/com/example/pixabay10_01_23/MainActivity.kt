07175869b8ad77b0949cdc37ae049298e2dfec6c
Pablo Sánchez López <65461723+PSL2001@users.noreply.github.com>
2023-01-10 14:31:06 +0100

    Actualizado ApiService.kt y Conectado recycler con la api

--- app/src/main/java/com/example/pixabay10_01_23/MainActivity.kt
+++ app/src/main/java/com/example/pixabay10_01_23/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.pixabay10_01_23
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.pixabay10_01_23.adapter.PixabayAdapter
+import com.example.pixabay10_01_23.apiProvider.ApiClient
+import com.example.pixabay10_01_23.databinding.ActivityMainBinding
+import com.example.pixabay10_01_23.models.Hit
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    var lista = mutableListOf<Hit>()
+    lateinit var adapter: PixabayAdapter
+    var key = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        key = getString(R.string.api_key)
+        setContentView(binding.root)
+        setRecyclerView()
+    }
+
+    private fun setRecyclerView() {
+        val layoutManager = LinearLayoutManager(this)
+        binding.recPix.layoutManager = layoutManager
+        adapter = PixabayAdapter(lista)
+        binding.recPix.adapter = adapter
+        traerImagenes()
+    }
+
+    private fun traerImagenes() {
+        lifecycleScope.launch{
+            val datos = ApiClient.service.getImages(key, "Aviones")
+
+            adapter.lista = datos.hits.toMutableList()
+            adapter.notifyDataSetChanged()
+        }
     }
 }
--- app/src/main/java/com/example/pixabay10_01_23/apiProvider/ApiService.kt
+++ app/src/main/java/com/example/pixabay10_01_23/apiProvider/ApiService.kt
@@ -2,9 +2,10 @@ package com.example.pixabay10_01_23.apiProvider
 
 import com.example.pixabay10_01_23.models.Pixabay
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiService {
     @GET("api/")
 
-    suspend fun getImages(): Pixabay
+    suspend fun getImages(@Query("key") key: String, @Query("q") q: String): Pixabay
 }
