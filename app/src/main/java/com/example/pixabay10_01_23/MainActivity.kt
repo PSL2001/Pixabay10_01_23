1fb8f94b20c10ef45de976bc59dbcac0320ce154
PSL2001 <65461723+PSL2001@users.noreply.github.com>
2023-01-11 21:30:13 +0100

    Proyecto terminado

--- app/src/main/java/com/example/pixabay10_01_23/MainActivity.kt
+++ app/src/main/java/com/example/pixabay10_01_23/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.pixabay10_01_23
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SearchView
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.pixabay10_01_23.adapter.PixabayAdapter
@@ -15,25 +16,40 @@ class MainActivity : AppCompatActivity() {
     var lista = mutableListOf<Hit>()
     lateinit var adapter: PixabayAdapter
     var key = ""
+    var term = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         key = getString(R.string.api_key)
         setContentView(binding.root)
         setRecyclerView()
+        setListeners()
     }
 
+    private fun setListeners() {
+        binding.svBusca.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                term = p0.toString()
+                traerImagenes(term)
+                return false
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                return false
+            }
+        })
+    }
     private fun setRecyclerView() {
         val layoutManager = LinearLayoutManager(this)
         binding.recPix.layoutManager = layoutManager
         adapter = PixabayAdapter(lista)
         binding.recPix.adapter = adapter
-        traerImagenes()
+
     }
 
-    private fun traerImagenes() {
+    private fun traerImagenes(termino: String) {
         lifecycleScope.launch{
-            val datos = ApiClient.service.getImages(key, "Aviones")
+            val datos = ApiClient.service.getImages(key, termino)
 
             adapter.lista = datos.hits.toMutableList()
             adapter.notifyDataSetChanged()
